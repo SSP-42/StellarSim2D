1f17e35d77f47b897a80cf4b905d862828eba93a
Archie Macdonald <SE6446@cyber-wizard.com>
2023-12-06 01:36:14 -0800

    fixed envs again

---
+++ Assets/ML_stuff/Orbiter.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using Random = UnityEngine.Random;
+
+public class Orbiter : Agent
+{
+    private Rigidbody rBody;
+    private properties properties;
+    [SerializeField]
+    private float smooth = 3f;
+    Vector3 originalPos;
+    public bool thrusting; // There has to be a better name for this.
+    private Quaternion originalRot;
+    void Start(){
+        Debug.Log("Assigning variables");
+        originalPos = gameObject.transform.position;
+        originalRot = gameObject.transform.rotation;
+        rBody = this.GetComponent<Rigidbody>();
+        properties = this.GetComponent<properties>();
+    }
+    public override void OnEpisodeBegin()
+    {
+        // reset rocket and create new target
+        rBody.velocity = Vector3.zero;
+        gameObject.transform.position = originalPos;
+        gameObject.transform.rotation = originalRot;
+
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        //*target and self positon
+        sensor.AddObservation(this.transform.localPosition);
+        //*current velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.y);
+        sensor.AddObservation(rBody.velocity.z);
+        //*current orientation
+        sensor.AddObservation(this.transform.rotation.x);
+        sensor.AddObservation(this.transform.rotation.y);
+        sensor.AddObservation(this.transform.rotation.z);
+    }
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        //*actions
+        Quaternion controlRotation = Quaternion.Euler(0,0,actionBuffers.ContinuousActions[0]);
+        transform.rotation = Quaternion.Slerp(this.transform.rotation,controlRotation,Time.deltaTime * smooth);  
+
+        if (actionBuffers.ContinuousActions[1] > 0f)
+        {
+            rBody.AddForce(transform.up*actionBuffers.ContinuousActions[1]*1000);
+            //thrusting = true;
+        }
+        else if (actionBuffers.ContinuousActions[1] == 0f)
+        {
+            //thrusting = false;
+        }
+        //* Rewards
+        float distanceToHome = Vector3.Distance(this.transform.localPosition,Vector3.zero);
+        if (false) //placeholder
+        {
+            EndEpisode();
+        }
+        else if (properties.contact == true){
+            Debug.Log("Crashlanding");
+            SetReward(-2.0f);
+            EndEpisode();
+        }
+        else if (distanceToHome > 100000f)
+        {
+            Debug.Log("Lost to the void");
+            EndEpisode();
+        }
+    }
+    public float z = 50f;
+    public float test_thrust = 100f;
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = z;
+        continuousActionsOut[1] = test_thrust;
+
+    }
+}
